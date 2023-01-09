893471a3877e1a0138b180e08403e6db4daa890e
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2023-01-09 21:37:36 +0700

    add html and serve on nodejs

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const fs = require('fs')
+
+const requestListener = (req, res) => {
+    const data = fs.readFileSync('./index.html')
+
+    res.end(data.toString());
+}
+
+const server = http.createServer(requestListener)
+
+const PORT = 8000
+const host = 'localhost'
+
+server.listen(PORT, host, () => {
+    console.log(`server running on http://${host}:${PORT}`)
+})
